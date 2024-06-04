ed7551d5ca4749c8ca349a8d10abecc9649de478
giacoren6 <142323106+giacoren6@users.noreply.github.com>
2024-06-04 16:46:32 +0000

    adding views in django project

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def index(request):
+    return render(request, "index.html")
+
+def book_done(request):
+    return render(request, "book_done.html")
+
+def book(request):
+    return render(request, "book.html")
+
+def login_done(request):
+    return render(request, "login_done.html")
+
+def login(request):
+    return render(request, "login.html")
+
+def rooms(request):
+    return render(request, "rooms.html")
+
+def signup(request):
+    return render(request, "signup.html")
+
+
